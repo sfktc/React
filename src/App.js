f6ad659f841581f8c291e785851be58ff6e16256
Oleg <tsoyoleg99@mail.ru>
2023-09-15 14:15:29 +0500

    without jsx

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { jsx as _jsx } from 'react/jsx-runtime';
+import { jsxs as _jsxs } from 'react/jsx-runtime';
 export const App = () => {
 	const date = new Date();
 	const formattedDate = date.getFullYear();
-	return (
-		//Декларативный код
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<h1>{formattedDate}</h1>
-			</header>
-		</div>
-	);
+	return _jsx('div', {
+		className: 'App',
+		children: _jsxs('header', {
+			className: 'App-header',
+			children: [
+				_jsx('img', {
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				}),
+				_jsxs('p', {
+					children: [
+						'Edit ',
+						_jsx('code', {
+							children: 'src/App.js',
+						}),
+						' and save to reload 2.',
+					],
+				}),
+				_jsx('a', {
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+					children: 'Learn React',
+				}),
+				_jsx('h1', {
+					children: formattedDate,
+				}),
+			],
+		}),
+	});
 };
